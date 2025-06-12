c2380291cfba72e8f48bc883860308efd233f163
poq <120293384+poqbox@users.noreply.github.com>
2025-06-12 19:04:23 -0400

    Added basic HATEOAS links

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const notes_routes = require("./routes/notes")
 const app = express()
 app.listen(3000, () => { console.log("Server running.") })
 app.use("/api/notes", notes_routes)
+
+
+app.get("/", (request, response) => {
+    // HATEOAS links
+    let links = [
+        {
+            href: "/api",
+            rel: "api",
+            type: "GET",
+        },
+    ]
+
+    response.json({ links })
+})
