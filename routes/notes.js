5904d7bc1f309b99ffe0b6c27858c598719ad3d5
poq <120293384+poqbox@users.noreply.github.com>
2025-06-12 21:38:52 -0400

    Implemented a search route

--- routes/notes.js
+++ routes/notes.js
@@ -25,7 +25,71 @@ router.route("/")
             next(error(400, "Insufficient Data"))
         }
     })
+
+router.route("/:id")
+    .get((request, response, next) => {
+        const id = request.params.id
+        const note = notes.find((_) => _.id == id)
+
+        if (note)
+            response.json(note)
+        else
+            next()
+    })
+    .patch((request, response, next) => {
+        const id = request.params.id
+        if (request.body.title || request.body.content) {
+            const note = notes.find((_) => {
+                if (_.id == id) {
+                    if (request.body.title) {_.title = request.body.title}
+                    if (request.body.content) {_.content = request.body.content}
+                    return true
+                }
+            })
+
+            if (note)
+                response.json(note)
+        }
+        next()
+    })
+    .delete((request, response, next) => {
+        const id = request.params.id
+        const note = notes.find((_, i) => {
+            if (_.id == id) {
+                notes.splice(i, 1)
+                return true
+            }
+        })
+
+        if (note)
+            response.json(note)
+        else
+            next()
+    })
+
+router.route("/search")
+    .get((request, response, next) => {
+        const title = request.query["title"]
+        const content = request.query["content"]
+        query_notes = search(title, content)
+
+        response.json(search(title, content))
+    })
 //
+
+
+function search(title, content) {
+    // search engine
+    const query_notes = []
+    const reTitle = new RegExp((title) ? String.raw`^.*${title}.*$` : String.raw`.*`)
+    const reContent = new RegExp((content) ? String.raw`^.*${content}.*$` : String.raw`.*`)
+    notes.forEach((_) => {
+        if (reTitle.test(_.title) && reContent.test(_.content))
+            query_notes.push(_)
+    })
+
+    return query_notes
+}
 
 
 module.exports = router
