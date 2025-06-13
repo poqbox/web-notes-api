d2622b3c44cbac3fd8cbc2d466ad3c293cfe5b90
poq <120293384+poqbox@users.noreply.github.com>
2025-06-12 21:38:03 -0400

    Added more example notes

--- data/notes.js
+++ data/notes.js
@@ -1,8 +1,18 @@
 const notes = [
   {
     id: 1,
-    title: "Example",
+    title: "Example 1",
     content: "..."
+  },
+  {
+    id: 2,
+    title: "Example 2",
+    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+  },
+  {
+    id: 3,
+    title: "Example 3",
+    content: "Mauris rutrum ex eu ipsum auctor, sit amet egestas neque sagittis."
   }
 ]
 
